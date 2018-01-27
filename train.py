e0a30744f1996979823c090c7fbfd900520ef2ab
manojd <manoj.ducher@gmail.com>
2018-01-27 23:51:41 +0100

    added comments

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ class Railroad:
         self.routes_graph={}
     
     def build(self, input_towns_string):
+        self.check_input_string(input_towns_string)
         input_towns_list = input_towns_string.split(", ")
         for e in input_towns_list:
             self.construct_graph(e)
@@ -24,42 +25,40 @@ class Railroad:
         l = 0
         distance = 0
         while l != len(route_string) - 1:
+#           Try important here to avoid KeyError Exception
             try:
                 if route_string[l+1] in self.routes_graph[route_string[l]]:
                     distance += self.routes_graph[route_string[l]][route_string[l+1]]
                 else:
                     distance = str('NO SUCH ROUTE')
                 l += 1
+#           If KeyError shout out if unknown key is inputed then capture the Exception
             except KeyError:
                 distance = str('NO SUCH ROUTE')
                 l += 1
         print(distance)
 
-    '''
     def check_input_string(self, string_distance):
         input_towns_list = string_distance.split(", ")
         length_input = len(input_towns_list)
         for l in range(0, length_input):
             input_distance = input_towns_list[l]
-            print(input_distance)
             i = 0
             for char in input_distance:
                 i += 1
-                if (len(input_distance) > 3) or (len(input_distance) < 3):
-                    raise Exception('input distance format should be 3 characters')
+                if (len(input_distance) < 3):
+                    raise Exception('input distance format should be at least 3 characters')
+#               First character must be a letter
                 elif (i == 1) and (char.isalpha() == True):
-                    print('first character is a letter')
                     pass
+#               Second character but be a letter
                 elif (i == 2) and (char.isalpha() == True):
-                    print('second character is a letter')
                     pass
-                elif (i == 3) and (char.isdigit() == True):
-                    print('third character is a number')
+#               Third character and fourth and so on can be digits (so combined are a number)
+                elif ( 3 <= i <= len(input_distance)) and (char.isdigit() == True):
                     pass
                 else:
-                    raise Exception('first and second characters should be letters and third a number')
-        return input_towns_list
-    '''
+                    raise Exception('first and second characters should be letters and the following a number')
     
 railroad = Railroad()
 routes_graph = railroad.build(input_towns_string)
@@ -67,6 +66,6 @@ route = railroad.calc_distance('A-B-C')
 route = railroad.calc_distance('A-D')
 route = railroad.calc_distance('A-D-C')
 route = railroad.calc_distance('A-E-B-C-D')
-route = railroad.calc_distance('A-Z-D')
+route = railroad.calc_distance('A-E-D')
 
 
