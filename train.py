1e90e2aba48ea72e3efd1a34774d0eebdaf21830
manojd <manoj.ducher@gmail.com>
2018-02-26 19:21:12 +0100

    modified code with better variable name

--- train.py
+++ train.py
@@ -1,6 +1,3 @@
-#from gui import GraphicalInterface
-#from tkinter import *
-
 input_towns_string = 'AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7'
 
 class Railroad:
@@ -11,27 +8,69 @@ class Railroad:
         _ a dictionary: routes_graph
         _ an int counting the number of routes between two towns
         _ a list containing possible routes between two towns.
+        The connected_towns input given by user "input_towns_string" is
+        checked for error.
+        And the graph is made.
         '''
         self.routes_graph = {}
         self.number_routes = 0
         self.possible_routes = []
+        self.input_towns_list = input_towns_string.split(", ")
+        self.check_input_string()
+        self.build()
 
-    def calc_distance(self, route_string):
+    def check_input_string(self):
+        '''
+        Method to check the input given to create the graph.
+        '''
+        for connected_towns in self.input_towns_list:
+            if (len(connected_towns) < 3):
+                raise Exception('input distance format should be at least 3 characters')
+            for i, char in enumerate(connected_towns):
+                if i == 0 or i == 1:
+                    if not char.isalpha():
+                        raise Exception('fist and second characters must be a letter')
+                elif not char.isdigit():
+                    raise Exception('first and second characters should be letters and the following a number')
+    
+    def build(self):
+        '''
+        Method that calls "construct_graph" to create the dictionary containing the graph.
+        '''
+        for connected_towns in self.input_towns_list:
+            self.construct_graph(connected_towns)
+
+    def construct_graph(self, connected_towns_string):
+        '''
+        Method to construct the graph.
+        '''
+        # If starting town is not in the graph add it.
+        if connected_towns_string[0] not in self.routes_graph:
+            self.routes_graph[connected_towns_string[0]] = {}
+        # Town connected to starting one is added to graph
+        if connected_towns_string[1]:
+            self.routes_graph[connected_towns_string[0]][connected_towns_string[1]] = {}
+        # Distance between the 2 towns is added to the graph. 
+        if connected_towns_string[2]:
+            self.routes_graph[connected_towns_string[0]][connected_towns_string[1]] = int(connected_towns_string[2:])
+            
+    def calc_distance(self, input_route_string):
         '''
         Method to calculate distance between two towns.
         '''
-        self.build(input_towns_string)
         # Input is given as 'A-B-C'
-        route_string = route_string.split("-")
+        input_route_list = input_route_string.split("-")
         counter = 0
         distance = 0
         # len(route_string) - 1 is the definition of number of stops
-        number_stops = len(route_string) - 1
+        number_stops = len(input_route_list) - 1
         while counter != number_stops:
+            actual_town = input_route_list[counter]
+            new_town = input_route_list[counter + 1]
         # Try/Except important here to avoid KeyError Exception.
             try:
-                if route_string[counter+1] in self.routes_graph[route_string[counter]]:
-                    distance += self.routes_graph[route_string[counter]][route_string[counter+1]]
+                if new_town in self.routes_graph[actual_town]:
+                    distance += self.routes_graph[actual_town][new_town]
                 else:
                     distance = str('NO SUCH ROUTE')
                 counter += 1
@@ -41,56 +80,10 @@ class Railroad:
                 counter += 1
         return(distance)
 
-    def build(self, input_towns_string):
-        '''
-        Method to check the input given to create the graph with "check_input_string method" 
-        and calls "construct_graph" method to create the dictionary containing the graph.
-        '''
-        self.check_input_string(input_towns_string)
-        input_towns_list = input_towns_string.split(", ")
-        for e in input_towns_list:
-            self.construct_graph(e)
-
-    def check_input_string(self, string_distance):
-        input_towns_list = string_distance.split(", ")
-        length_input = len(input_towns_list)
-        for l in range(0, length_input):
-            input_distance = input_towns_list[l]
-            i = 0
-            for char in input_distance:
-                i += 1
-                if (len(input_distance) < 3):
-                    raise Exception('input distance format should be at least 3 characters')
-                # First character must be a letter
-                elif (i == 1) and (char.isalpha() == True):
-                    pass
-                # Second character but be a letter
-                elif (i == 2) and (char.isalpha() == True):
-                    pass
-                # Third character and fourth and so on can be digits (so combined are a number)
-                elif ( 3 <= i <= len(input_distance)) and (char.isdigit() == True):
-                    pass
-                else:
-                    raise Exception('first and second characters should be letters and the following a number')
-
-    def construct_graph(self, input_towns_list):
-        '''
-        Method to construct the graph.
-        '''
-        # If starting town is not in the graph add it.
-        if input_towns_list[0] not in self.routes_graph:
-            self.routes_graph[input_towns_list[0]] = {}
-        # Town connected to starting one is added to graph
-        if input_towns_list[1]:
-            self.routes_graph[input_towns_list[0]][input_towns_list[1]] = {}
-        # Distance between the 2 towns is added to the graph. 
-        if input_towns_list[2]:
-            self.routes_graph[input_towns_list[0]][input_towns_list[1]] = int(input_towns_list[2:])
-
     def display_all_routes(self, start, end, number_stops, method, path=[]):
         '''
         Method to display all possible routes given the starting, ending town 
-        and maximum number of stops. We check with variable method that we need 
+        and the number of stops. We check that we should make the trip within 
         a maxmimum number of stops "<" or exactly a number of stops "=".
         '''
         path = path + [start]
@@ -117,19 +110,18 @@ class Railroad:
 
     def count_number_routes(self, start, end, number_stops, method):
         '''
-        Method calling the main method (display_all_routes) and outputing the number of routes.
+        Method calling the method "display_all_routes" and outputing the number of routes
+        between two towns.
         '''
-        self.build(input_towns_string)
         self.number_routes = 0
         self.display_all_routes(start, end, number_stops, method)
         return self.number_routes
 
     def shortest_route_display(self, start, end, number_stops, method):
         '''
-        Method calling the main method (display_all_routes) and for all possible
-        routes computing the total distance between starting and ending towns.
+        Method calling the method "display_all_routes" and for all possible
+        routes computing the shortest distance between starting and ending towns.
         '''
-        self.build(input_towns_string)
         self.possible_routes = []
         all_distances = []
         self.display_all_routes(start, end, number_stops, method)
@@ -137,7 +129,7 @@ class Railroad:
         for e in self.possible_routes:
             distance = 0
             # This is a way to select AB, BC, CD
-            for f,g in zip(e,e[1:]):
+            for f, g in zip(e, e[1:]):
                 # Compute the distance for each couple using the graph
                 distance += self.routes_graph[f][g]
             # Save distances in a list
@@ -150,16 +142,15 @@ class Railroad:
 
     def number_different_routes(self, start, end, number_stops, method):
         '''
-        Method calling the main method (display_all_routes) and counting the number
-        of different routes having a length smaller than 30.
+        Method calling the method "display_all_routes" and counting the number
+        of different routes between 2 towns having a length smaller than 30.
         '''
-        self.build(input_towns_string)
         self.possible_routes = []
         all_distances = []
         self.display_all_routes(start, end, number_stops, method)
         for e in self.possible_routes:
             distance = 0
-            for f,g in zip(e,e[1:]):
+            for f, g in zip(e, e[1:]):
                 distance += self.routes_graph[f][g]
             all_distances += [distance]
         # We use here the index correspondance between possible.routes and all_distances.
